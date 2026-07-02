638a4bf5d52a6d1ff07c7680885d4564160504c8
samlouise <samlouise.morgado.25@usjr.edu.ph>
2026-07-02 12:04:05 +0800

    Update Palindrome checker.cpp

--- Palindrome checker.cpp
+++ Palindrome checker.cpp
@@ -2,28 +2,23 @@
 
 int isPalindrome(int arr[], int size)
 {
-    int i;
-
-    for (i = 0; i < size / 2; i++)
+    for (int i = 0; i < size / 2; i++)
     {
-        if (arr[i] != arr[size - i - 1])
+        if (arr[i] != arr[size - 1 - i])
             return 0;
     }
-
     return 1;
 }
 
 void printArray(int arr[], int size)
 {
-    int i;
-
     printf("{");
 
-    for (i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         printf("%d", arr[i]);
 
-        if (i < size - 1)
+        if (i != size - 1)
             printf(", ");
     }
 
@@ -33,10 +28,30 @@ void printArray(int arr[], int size)
 int main()
 {
     int arr1[] = {1, 2, 3, 2, 1};
+    int arr2[] = {5, 4, 4, 5};
+    int arr3[] = {1, 2, 3, 4, 5};
+    int arr4[] = {7};
 
     printArray(arr1, 5);
-
     if (isPalindrome(arr1, 5))
+        printf(" = PALINDROME\n");
+    else
+        printf(" = NOT PALINDROME\n");
+
+    printArray(arr2, 4);
+    if (isPalindrome(arr2, 4))
+        printf(" = PALINDROME\n");
+    else
+        printf(" = NOT PALINDROME\n");
+
+    printArray(arr3, 5);
+    if (isPalindrome(arr3, 5))
+        printf(" = PALINDROME\n");
+    else
+        printf(" = NOT PALINDROME\n");
+
+    printArray(arr4, 1);
+    if (isPalindrome(arr4, 1))
         printf(" = PALINDROME\n");
     else
         printf(" = NOT PALINDROME\n");
